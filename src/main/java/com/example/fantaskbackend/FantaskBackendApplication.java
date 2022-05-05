abf9cb6a5baca987113acd199b2867634edcefea
kamillenikolajsen <knikolajsen4@gmail.com>
2022-05-05 09:26:33 +0200

    Initial commit

---
+++ src/main/java/com/example/fantaskbackend/FantaskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fantaskbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantaskBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FantaskBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/fantaskbackend/FantaskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fantaskbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantaskBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
