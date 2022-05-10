ea5a60df58bc1b4f91795907d3bede8d11089466
Michala Nybroe <michalanim@gmail.com>
2022-05-10 22:33:10 +0200

    book model

--- src/main/java/com/example/fantaskbackend/model/Book.java
+++ src/main/java/com/example/fantaskbackend/model/Book.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Getter
 @Setter
@@ -12,8 +13,76 @@ public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long bookId;
 
-    //@Column(nullable = false)
+    @Column(nullable = false, unique = true)
+    private String ISBN;
+
+    @Column(name = "fk_serie")
+    private Long series;
+
+    @Column(name = "fk_forfatter")
+    private Long author;
+
+    @Column(name = "fk_forlag")
+    private Long publisher;
+
+    @Column(name = "fk_kategori")
+    private Long category;
+
+    @Column(name = "fk_genre")
+    private Long genre;
+
+    @Column(name = "fk_lager")
+    private Long storage;
+
+    @Column(name = "nummer")
+    private String number;
+
+    @Column(name = "titel")
+    private String title;
+
+    @Column(name = "original_pris")
+    private String originalPrice;
+
+    @Column(name = "dansk_pris")
+    private String danishPrice;
+
+    @Column(name = "udgivelsesår")
+    private String yearOfPublication;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "beskrivelse")
+    private String description;
+
+    @Column(name = "sider")
+    private int pages;
+
+    @Column(nullable = false, name = "dato")
+    private Date date;
+
+    @Column(nullable = false, name = "udgået")
+    private boolean unavailable;
+
+    @Column(nullable = false, name = "kommende")
+    private boolean coming;
+
+    @Column(nullable = false, name = "abonnement")
+    private boolean subscription;
+
+    @Column(nullable = false, name = "restordre")
+    private boolean backorder;
+
+    @Column(nullable = false, name = "udsolgt")
+    private boolean outOfStock;
+
+    @Column(nullable = false, name = "skjul")
+    private boolean hidden;
+
+    @Column(nullable = false, name = "tilbud")
+    private boolean onSale;
 
 }
