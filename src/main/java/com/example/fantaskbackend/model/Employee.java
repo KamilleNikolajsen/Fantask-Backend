b175e72a15035e0b432e41cd84789786b80e6e9e
Michala Nybroe <michalanim@gmail.com>
2022-05-11 09:55:19 +0200

    add employee

--- src/main/java/com/example/fantaskbackend/model/Employee.java
+++ src/main/java/com/example/fantaskbackend/model/Employee.java
@@ -3,17 +3,24 @@ package com.example.fantaskbackend.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity
+@Entity(name = "ansatte")
 @Setter
 @Getter
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long employeeId;
+
+    @Column(name = "navn")
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
 }
--- src/main/java/com/example/fantaskbackend/model/Game.java
+++ src/main/java/com/example/fantaskbackend/model/Game.java
@@ -3,17 +3,15 @@ package com.example.fantaskbackend.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity
+@Entity(name = "spil")
 @Getter
 @Setter
 public class Game {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long gameId;
 }
