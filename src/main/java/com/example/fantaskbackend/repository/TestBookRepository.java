1689fd874133a875f449a1d7c6e778a02f273032
kamillenikolajsen <knikolajsen4@gmail.com>
2022-05-18 13:51:32 +0200

    Add futher testing

--- src/main/java/com/example/fantaskbackend/repository/TestBookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.fantaskbackend.repository;
-
-import com.example.fantaskbackend.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestBookRepository extends JpaRepository<Book, Long> {
-
-  Book findBookByTitle(String name);
-}
--- src/test/java/com/example/fantaskbackend/controller/BookRESTControllerTest.java
+++ src/test/java/com/example/fantaskbackend/controller/BookRESTControllerTest.java
@@ -2,24 +2,59 @@ package com.example.fantaskbackend.controller;
 
 import com.example.fantaskbackend.model.Book;
 import com.example.fantaskbackend.repository.BookRepository;
-import com.example.fantaskbackend.repository.TestBookRepository;
 import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class BookRESTControllerTest {
 
   @Autowired
-  TestBookRepository testBookRepository;
+  private TestEntityManager testEntityManager;
 
+  @Autowired
+  BookRepository bookRepository;
+
+  // VIRKER IKKE
+  @Test
+  public void readBook(){
+    Book book = new Book();
+    Date date = new Date(2022,12,12);
+
+    book.setTitle("Test Titel");
+    book.setISBN("Test ISBN");
+    book.setUnavailable(true);
+    book.setDate(date);
+    book.setComing(true);
+    book.setSubscription(true);
+    book.setBackorder(true);
+    book.setOutOfStock(true);
+    book.setHide(true);
+    book.setOnSale(true);
+
+    bookRepository.save(book);
+
+    assertThat(book).hasFieldOrPropertyWithValue("titel", "Test Titel");
+    assertThat(book).hasFieldOrPropertyWithValue("ISBN", "Test ISBN");
+
+  }
+
+  /*
   @Test
   public void saveBook(){
     //Arrange
     Book book = new Book();
     book.setTitle("Test");
-    Book expected = testBookRepository.findBookByTitle("Test");
+    Book expected = bookRepository.
 
     //Act
     testBookRepository.save(book);
@@ -27,5 +62,6 @@ class BookRESTControllerTest {
     //Asses
     assertEquals(expected, book);
   }
+   */
 
 }
